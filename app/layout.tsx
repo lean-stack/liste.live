5ed92aedc93cbc89250103350c4328b327ac15e8
Micha Buchholz <mail@lean-stack.de>
2023-06-08 10:36:01 +0200

    feat: Enable dark mode based on client prefs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,13 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="de" className="h-full dark">
+    <html lang="de" className="h-full" suppressHydrationWarning>
       <head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" type="image/png" href="/favicon.png" />
       </head>
       <body className={cn("h-full antialiased", inter.className)}>
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
